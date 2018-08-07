65bbb0faa32f00f271fbd26bb0ba5719f9768338
huanghj8 <huanghj8@163.com>
2018-08-07 08:53:58 +0800

    [WCDMA]configure downlink test environment

--- my_logging.py
+++ my_logging.py
@@ -1,21 +1,20 @@
 import logging
-
 import time
 
 
 class MyLogging(object):
     def __init__(self):
+        self.formatter = logging.Formatter(
+            '%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+        self.console_handler = logging.StreamHandler()
+        self.file_handler = logging.FileHandler('result\Testlog_%s.log' % self.local_time)
         self.local_time = time.strftime("%Y%m%d-%H%M%S")
 
     def get_logger(self):
         logger = logging.getLogger("Logger")
         logger.setLevel(logging.DEBUG)
-        self.file_handler = logging.FileHandler('result\Testlog_%s.log' % self.local_time)
         self.file_handler.setLevel(logging.DEBUG)
-        self.console_handler = logging.StreamHandler()
         self.console_handler.setLevel(logging.DEBUG)
-        self.formatter = logging.Formatter(
-            '%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
         self.file_handler.setFormatter(self.formatter)
         self.console_handler.setFormatter(self.formatter)
         logger.addHandler(self.file_handler)
--- wcdma_throughput.py
+++ wcdma_throughput.py
@@ -28,6 +28,7 @@ class WcdmaThroughput(object):
             [5, [4358, 4400, 4457]],  # band5
             [8, [2938, 3013, 3087]]  # band8
         ]
+        self.chip_set = "MTK"
 
     def write(self, command):
         self.my_instr.write(command)
@@ -41,7 +42,7 @@ class WcdmaThroughput(object):
     def handover(self, channel, band=1, channel_type="low/mid"):
         if channel:
             self.logger.info("band: %s, channel: %s", str(band), str(channel))
-            self.write("CALL:SETup:CHANnel:DOWNlink %s" %str(channel))
+            self.write("CALL:SETup:CHANnel:DOWNlink %s" % str(channel))
             # self.write("CALL:SETup:CHANnel:DOWNlink %s" % str(channel))
         # print self.read()
         if band == 5:
@@ -87,7 +88,103 @@ class WcdmaThroughput(object):
         self.write("SYSTEM:CORRECTION:GAIN -0.50,-0.80")
 
     def set_downlink_environment(self):
+        """
+        配置下行吞吐量测试环境
+        :return: null
+        """
+        # preset
+        self.logger.info("preset")
+        self.write("*RST")
+        self.write("CALL:BCCHannel:UPDAtepage AUTO")
+        self.write("CALL:ATTFlag ON")
+        self.write("CALL:UPLink:TXPower:LEVel:MAXimum 24")
+        # set Authentication code
+        self.logger.info("set Authentication code")
+        self.write("CALL:SECurity:AUTHenticate:KEY " +
+                   "'000102030405060708090A0B0C0D0E0F'")
+        # set RRC Reesablish to Auto for MTK, and Off for QC
+        if self.chip_set == "QC":
+            self.write("CALL:CELL:RLC:REEStablish OFF")
+        else:
+            self.write("CALL:CELL:RLC:REEStablish AUTO")
+        # set attenuation
+        self.logger.info("set attenuation")
         self.set_cable_loss()
+        self.write("CALL:OPERating:MODE OFF")
+        time.sleep(1)
+        self.write("CALL:SERVice:RBTest:RAB HSDP12")
+        # TODO find instruction to set E6.2.3.4
+        self.write("")
+        self.write("CALL:CCPChannel:SECondary:CONNected:CONFig:STATe OFF")
+        time.sleep(1)
+        # set test mode parameter
+        self.logger.info("set test mode parameter")
+        self.write("CALL:HSDPa:SERVice:RBTest:HSDSchannel:CONFig UDEFined")
+        self.write("CALL:HSDPa:SERVice:RBTest:UDEFined:HSDSchannel:MAC EHSPeed")  # enhanced high speed
+        self.write("CALL:HSDPa:SERVice:RBTest:UDEFined:HARQ:PROCess:COUNt 6")
+        self.write("CALL:HSDPa:SERVice:RBTest:UDEFined:MS:IREDundancy:BUFFer:ALLocation IMPLicit")
+        self.write("CALL:HSDPa:SERVice:RBTest:UDEFined:DCHSdpa ON")
+        self.write("CALL:HSDPa:SERVice:RBTest:DCHSdpa:DPCH:LOOPback ON")
+        time.sleep(1)
+        # set server cell parameter
+        self.logger.info("set server cell parameter")
+        self.write("CALL:HSDPa:SERVice:RBTest:UDEFined:QAM64:STATe ON")
+        self.write("CALL:HSDPa:SERVice:RBTest:UDEFined:HSPDschannel:COUNt 15")
+        self.write("CALL:HSDPa:SERVice:RBTest:UDEFined:TBSize:INDex 62")
+        self.write("CALL:HSDPa:SERVice:RBTest:UDEFined:MODulation[:TYPE] QAM64")
+        self.write("CALL:HSDPa:SERVice:RBTest:UDEFined:ITTI[:INTerval] 1")
+        time.sleep(1)
+        # set sencondary cell parameter
+        self.logger.info("set secondary cell parameter")
+        self.write("CALL:HSDPa:SSCell:RBTest:UDEFined:QAM64:STATe ON")
+        self.write("CALL:HSDPa:SSCell:RBTest:UDEFined:HSPDschannel:COUNt 15")
+        self.write("CALL:HSDPa:SSCell:RBTest:UDEFined:TBSize:INDex 62")
+        self.write("CALL:HSDPa:SSCell:RBTest:UDEFined:MODulation QAM64")
+        self.write("CALL:HSDPa:SSCell:RBTest:UDEFined:ITTI 1")
+        # set power
+        self.logger.info("set power")
+        self.write("CALL:SSCell:POWer -60 DBM")
+        self.write("CALL:CELL:POWer -60 DBM")
+        # set downlink channel level
+        self.logger.info("set downlink channel level")
+        self.write("CALL:CONNected:CPIChannel:HSDPa -8")
+        self.write("CALL:CONNected:CPIChannel:PRIMary:HSDPa -20")  # -20 is the lowest
+        self.write("CALL:CONNected:PICHannel:STATe:HSDPa OFF")  # use state instrution to set OFF
+        self.write("CALL:CONNected:DPCHannel:HSDPa -30")
+        self.write("CALL:CONNected:HSPDschannel -1")
+        self.write("CALL:CONNected:HSSCchannel[1] -20")
+        self.write("CALL:CONNected:HSSCchannel2 -20")
+        self.write("CALL:CONNected:HSSCchannel3 -15")  # -15 means off
+        self.write("CALL:CONNected:HSSCchannel4 -15")  # -15 means off
+
+        self.write("CALL:SSCell:CONNected:CPIChannel:LEVel:HSDPa -8")
+        self.write("CALL:SSCell:CONNected:CCPChannel:PRIMary:LEVel:HSDPa -20")  # -20 is the lowest
+        self.write("CALL:SSCell:CONNected:PICHannel:STATe:HSDPa OFF")  # use state instrution to set OFF
+        self.write("CALL:SSCell:CONNected:HSPDschannel:LEVel:HSDPa -1")
+        self.write("CALL:SSCell:CONNected:HSSCchannel1:LEVel:HSDPa -20")
+        self.write("CALL:SSCell:CONNected:HSSCchannel2:LEVel:HSDPa -20")
+        time.sleep(1)
+
+        # origin call
+        i = 3
+        while i > 0:
+            self.write("CALL:ORIGinate")
+            self.logger.info("wait for connect...")
+            time.sleep(5)
+            self.write("CALL:STAT?")
+            status = str(self.read())
+            self.logger.info("status:" + status)
+            if status == "CONN\n":
+                break
+            else:
+                time.sleep(1)
+                i -= 1
+
+    def recall_dl_register(self):
+        self.write("SYSTem:REGister:RECall 6")
+
+    def recall_ul_register(self):
+        self.write("SYSTem:REGister:RECall 4")
 
     def set_uplink_environment(self):
         self.set_cable_loss()
@@ -142,8 +239,12 @@ class WcdmaThroughput(object):
         self.write("CALL:SETup:CHANnel:DOWNlink 10700")
         self.write("CALL:HAND:PCR")
 
+    def test_instrution(self):
+        self.write("CALL:CONNected:PICHannel:STATe:HSDPa OFF ")  # use state instrution to set OFF
+
 
 if __name__ == "__main__":
     test_case = WcdmaThroughput()
     # test_case.case_all_uplink()
-    test_case.case_all_uplink()
+    # test_case.case_all_downlink()
+    test_case.recall_dl_register()
